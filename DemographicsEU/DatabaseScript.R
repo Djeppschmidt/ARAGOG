80429f2db219205eff0520ceeb177f20dde5258b
Dietrich Epp Schmidt <djeppschmidt@gmail.com>
2019-10-15 14:50:19 +0200

    update values
    update formating

--- DemographicsEU/DatabaseScript.R
+++ DemographicsEU/DatabaseScript.R
@@ -17,19 +17,27 @@ incomePcap.w<-incomePcap.w[,-1]
 incomePcap.w[] <- lapply(incomePcap.w, function(x) as.numeric(gsub(",","", x)))
 D.income<-data.frame("2009"=incomePcap.w$`2009`-incomePcap.w$`2008`, "2010"=incomePcap.w$`2010`-incomePcap.w$`2009`, "2011"=incomePcap.w$`2011`-incomePcap.w$`2010`, "2012"= incomePcap.w$`2012`-incomePcap.w$`2011`, "2013"=incomePcap.w$`2013`-incomePcap.w$`2012`, "2014"=incomePcap.w$`2014`-incomePcap.w$`2013`, "2015"=incomePcap.w$`2015`-incomePcap.w$`2014`,"2016"=incomePcap.w$`2016`-incomePcap.w$`2015`, "2017"=incomePcap.w$`2017`-incomePcap.w$`2016`)
 heatmap(as.matrix(D.income), Rowv=NA, Colv="Rowv", scale = "none",col=pal, main="Delta per cap. income")
-saveRDS(incomePcap.w, "~/Documents/GitHub/ARAGOG/DemographicsEU/PerCapIncome/PerCapIncome.RDS")
+saveRDS(D.income, "~/Documents/GitHub/ARAGOG/DemographicsEU/PerCapIncome/PerCapIncome.RDS")
 
 RegionAgIncome<-read.csv("~/Documents/GitHub/ARAGOG/DemographicsEU/AgIncome/nama_10r_2coe_1_Data.csv") # agricultural income per region in million EU
 AgIncome.w<-dcast(RegionAgIncome, GEO~TIME, value.var="Value")
 make.heatmap(AgIncome.w, "Delta Agricultural Income")
+AgIncome.w[] <- lapply(AgIncome.w, function(x) as.numeric(gsub(",","", x)))
+AgIncome.w<-AgIncome.w[,-1]
 saveRDS(AgIncome.w, "~/Documents/GitHub/ARAGOG/DemographicsEU/AgIncome/AgIncome.RDS")
 
 Employement<-read.csv("~/Documents/GitHub/ARAGOG/DemographicsEU/EmployementKhr/nama_10r_2emhrw_1_Data.csv") # per region, thousands of hours
 Employement.w<-dcast(Employement, GEO~TIME, value.var="Value")
 make.heatmap(Employement.w, "Delta Employement")
-saverds(Employement.w, "~/Documents/GitHub/ARAGOG/DemographicsEU/EmployementKhr/Employement.RDS")
+Employement.w[] <- lapply(Employement.w, function(x) as.numeric(gsub(",","", x)))
+Employement.w<-Employement.w[,-1]
+saveRDS(Employement.w, "~/Documents/GitHub/ARAGOG/DemographicsEU/EmployementKhr/Employement.RDS")
 
 Poverty<-read.csv("~/Documents/GitHub/ARAGOG/DemographicsEU/Poverty_percent/ilc_lvhl21_1_Data.csv")
 Poverty.w<-dcast(Poverty, UNIT+GEO~TIME, value.var="Value")
+Poverty.w<-Poverty.w[Poverty.w$UNIT=="Percentage of total population aged less than 60",]
+rownames(Poverty.w)<-Poverty.w$GEO
 make.heatmap(Poverty.w, "Delta poverty") # needs some work...
+Poverty.w[] <- lapply(Poverty.w, function(x) as.numeric(gsub(",","", x)))
+Poverty.w<-Poverty.w[,-c(1,2)]
 saveRDS(Poverty.w, "~/Documents/GitHub/ARAGOG/DemographicsEU/Poverty_percent/Poverty.RDS")
